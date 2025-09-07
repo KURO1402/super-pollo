0715c125b505acadbfa4c191a3acb6764d402608
PabloDev <yauripablo70@gmail.com>
2025-09-07 12:09:12 -0500

    Se optimizo y mejoro el modelo de insertar nuevo usuario

--- backend/src/modulos/usuarios/usuarioModelo.js
+++ backend/src/modulos/usuarios/usuarioModelo.js
@@ -1,37 +1,55 @@
-//importamos al conexion a base de datos
+//Importamos al conexion a base de datos
 const pool = require("../../config/conexionDB");
 
-//Esto es la funcion para registrar usuario usando el procedimiento almacenado
-const UsuarioModel = {
-    insertar : async(datos, claveEncriptada) => {
-        const {
-            nombresUsuario,
-            apellidosUsuario,
-            correoUsuario,
-            clave,
-            numeroDocumentoUsuario,
-            telefonoUsuario,
-            idRol,
-            idTipoDocumento
-        } = datos
+//Esto es la funcion para registrar usuario que recibe como parametros un objeto con los datos y la clave encriptada
+const insertarUsuarioDB = async ( datos, claveEncriptada) => {
+    //Usamos desestructuracion para obtener los valores del objeto datos
+        const { nombresUsuario, 
+                apellidosUsuario, 
+                correoUsuario, 
+                numeroDocumentoUsuario, 
+                telefonoUsuario, 
+                idTipoDocumento } = datos;
 
-        //llamamos al procedimiento almacenado
-        const[result] = await pool.query(
-            "CALL insertarUsuario(?, ? ,?, ?, ?, ?, ?, ?)",
-            [
-                nombresUsuario,
-                apellidosUsuario,
-                correoUsuario,
-                claveEncriptada,
-                numeroDocumentoUsuario,
-                telefonoUsuario,
-                idRol,
-                idTipoDocumento,
-            ]
-        );
-        //retonamos el resultado paar el controlador
-        return result
-    },
+        let conexion;//Variable para guardar la conexion a la base de datos
+        try {
+            conexion = await pool.getConnection() //Ejecutamos y guardamos en la variable la conexion a la db
+
+            // Iniciamos transacción para asegurarnos que se complete todo o nada 
+            await conexion.beginTransaction();
+
+            //llamamos y ejecutamos el procedimiento almacenado creado en la base de datos
+            const[result] = await conexion.execute(
+                "CALL insertarUsuario(?, ? ,?, ?, ?, ?, ?, ?)",
+                [
+                    nombresUsuario,
+                    apellidosUsuario,
+                    correoUsuario,
+                    claveEncriptada,
+                    numeroDocumentoUsuario,
+                    telefonoUsuario,
+                    idTipoDocumento,
+                ]
+            );
+
+            //Confirmamos los cambios o modificaciones hechas
+            await conexion.commit(); 
+    
+            //retonamos el resultado paar el controlador
+            return result
+        } catch (err){
+            // Si algo falla, revertimos cambios
+            if (conexion) await conexion.rollback();
+            //Mostramos el error
+            console.error("Error al insertar usuario:", err.message);
+            //Creamos un error para manejarlo en el controlador
+            throw new Error("Error al insertar usuario en la base de datos");
+        } finally {
+            //Liberamos la conexion
+            if(conexion) await conexion.release();
+        }
+}
+
+module.exports = {
+    insertarUsuarioDB
 };
-
-module.exports = UsuarioModel;
