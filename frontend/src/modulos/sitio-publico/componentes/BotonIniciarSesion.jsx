ddfaed6b89692b029ca16e9b5225754ed757c69f
Josue HT <jhuarocctapia@gmail.com>
2025-10-21 10:59:43 -0500

    Refactorización de la gestión de roles con redirecciones basadas en permisos

--- frontend/src/modulos/sitio-publico/componentes/BotonIniciarSesion.jsx
+++ frontend/src/modulos/sitio-publico/componentes/BotonIniciarSesion.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const BotonIniciarSesion = () => {
     return ( 
         <Link to='/inicio-sesion'>
-            <button className="text-gray-100 text-sm md:text-base hover:text-rojo transition-colors duration-200 px-2 py-1 rounded-md hover:bg-azul-primario">
+            <button className="text-gray-100 text-sm md:text-base hover:text-rojo transition-colors duration-200 px-2 py-1 rounded-md hover:bg-azul-primario cursor-pointer">
                 Iniciar Sesión
             </button>
         </Link>
--- frontend/src/modulos/sitio-publico/componentes/BotonRegistro.jsx
+++ frontend/src/modulos/sitio-publico/componentes/BotonRegistro.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const BotonRegistro = () => {
     return ( 
         <Link to='/registro'>
-            <button className="text-gray-100 text-sm md:text-base hover:text-rojo transition-colors duration-200 px-2 py-1 rounded-md hover:bg-azul-primario">
+            <button className="text-gray-100 text-sm md:text-base hover:text-rojo transition-colors duration-200 px-2 py-1 rounded-md hover:bg-azul-primario cursor-pointer">
                 Registrarse
             </button>
         </Link>
